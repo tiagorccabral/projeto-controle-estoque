04afb2e22803d2f3227bde5037825061ed561c3f
Tiago Cabral <150150296@aluno.unb.br>
2018-11-05 13:29:59 -0200

    Correções no template.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( vendors/iconfonts/mdi/css/materialdesignicons.min.css )
+Rails.application.config.assets.precompile += %w( vendors/css/vendor.bundle.base.css )
+Rails.application.config.assets.precompile += %w( vendors/css/vendor.bundle.addons.css )
+Rails.application.config.assets.precompile += %w( css/style.css )
+
