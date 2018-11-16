eb3df8421ca68ce0ae4a9524e1a21c6e99c8fe43
Tiago Cabral <150150296@aluno.unb.br>
2018-11-16 21:50:28 -0200

    CRUD de itens completo.

---
+++ app/controllers/backoffice/items_controller.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+class Backoffice::ItemsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @items = Item.all
+    @total_value = 0.0
+    @total_amount = 0
+    @items.each do |item|
+      @total_amount += item.amount
+      @total_value += item.value.to_f * item.amount.to_i
+    end
+  end
+
+  def items_list
+    @items = Item.all.order('updated_at DESC')
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to backoffice_items_path, notice: 'Item foi criado com sucesso.' }
+        format.js { }
+      else
+        format.html { render :new, notice: "Ocorreu um erro. Por favor, preencha todos os campos do formulário." }
+        format.js {  }
+      end
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to backoffice_items_list_path, notice: "Item foi atualizado com sucesso." }
+        format.js { }
+      else
+        format.html { render :edit, notice: "Ocorreu um erro. Por favor, preencha todos os campos do formulário." }
+        format.js {  }
+      end
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to backoffice_items_path, notice: 'Doação deletada com sucesso.' }
+      format.js {}
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:id, :name, :amount, :value, :donor, :receiver,
+                                 :internal, :used, :lost, :used_product, :new_product)
+  end
+end
---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+class Item < ApplicationRecord
+
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :value, presence: true, numericality: {greater_than: 0}
+  validates :donor, presence: false
+  validates :receiver, presence: true
+  validates :internal, presence: true
+end
