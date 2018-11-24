617ece708cd63b58fd09692bfd0ab2b990e981ac
Artur Henrique <arturhenrique71@gmail.com>
2018-11-24 18:29:23 -0200

    arrumando header backoffice

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Backoffice::DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @items = Item.all
+    @items.each do |item|
+      @total_amount += item.amount
+      @total_value += item.value.to_f * item.amount.to_i
+    end
   end
 end
