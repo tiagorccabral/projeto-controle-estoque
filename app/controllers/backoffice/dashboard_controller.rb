66dc6e46f5a1c5452c7d52a193185a6b4dde0405
Artur Henrique <arturhenrique71@gmail.com>
2018-11-26 23:43:19 -0200

    Colocando valores decentes no dashboard como total de vendas e valor arrecadado

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -4,9 +4,13 @@ class Backoffice::DashboardController < ApplicationController
     @items = Item.all
     @total_amount =0
     @total_value =0
+    @value_sold = 0.0
+    @total_sold = 0
     @items.each do |item|
       @total_amount += item.amount
       @total_value += item.value.to_f * item.amount.to_i
+      @value_sold += item.value_sold.to_f
+      @total_sold += item.total_sold.to_i
     end
   end
 end
